e4e64990462c499cf8b047e0848926ff92e843cd
Mark Hurwitz <mhurwitzus@gmail.com>
2017-07-07 14:49:19 -0500

    Adds method to seed hello-world project

--- imports/api/Projects/methods.js
+++ imports/api/Projects/methods.js
@@ -1,7 +1,39 @@
 import { Meteor } from 'meteor/meteor';
+import { check, Match } from 'meteor/check';
 import Projects from './Projects';
+import helloWorldProjectData from '../../startup/server/hello-world-project-data';
 
-Meteor.methods({});
+Meteor.methods({
+  'projects.seed': function seedProject(opts) {
+    // To overwrite the default 'hello-world' project:
+    // Meteor.call('projects.seed', { force: true })
+    // TODO: authorize only admins
+
+    check(opts, {
+      slug: Match.Maybe(String),
+      force: Match.Maybe(Boolean),
+    });
+
+    const slug = opts.slug || 'hello-world';
+    console.log('slug: ', slug);
+
+    const projectDoc = Projects.findOne({ slug });
+    console.log({ projectDoc });
+
+    if (projectDoc && !opts.force) {
+      console.log('project already exists. will not overwrite.');
+      return;
+    }
+
+    // data defaults to 'hello-world' project
+    let data;
+    if (slug == 'hello-world') data = helloWorldProjectData;
+
+    const result = Projects.insert(data);
+    console.log({ result });
+    return result;
+  },
+});
 
 // rateLimit({
 //   methods: [

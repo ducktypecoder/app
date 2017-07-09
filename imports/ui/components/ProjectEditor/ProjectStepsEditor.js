1604666a07c543161e181fc7aee3289e53089f20
Mark Hurwitz <mhurwitzus@gmail.com>
2017-07-09 14:29:49 -0500

    Begins adding a WYSIWYG editor to the step input

--- imports/ui/components/ProjectEditor/ProjectStepsEditor.js
+++ imports/ui/components/ProjectEditor/ProjectStepsEditor.js
@@ -4,12 +4,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormGroup, ControlLabel, Button } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { Bert } from 'meteor/themeteorchef:bert';
 import validate from '../../../modules/validate';
 
+const wrapperStyle = {
+  color: 'blue',
+};
+
+const editorStyle = {
+  border: '1px solid red',
+};
+
+const toolbarStyle = {
+  color: 'green',
+};
+
 class ProjectStepsEditor extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
+  }
+
+  onEditorStateChange(editorState) {
+    this.setState({ editorState });
+  }
+
   render() {
-    return <p>STEPS GO HERE</p>;
+    const { editorState } = this.state;
+    return (
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={this.onEditorStateChange}
+        wrapperClassName="wrapper-class"
+        editorClassName="editor-class"
+        toolbarClassName="toolbar-class"
+        wrapperStyle={wrapperStyle}
+        editorStyle={editorStyle}
+        toolbarStyle={toolbarStyle}
+      />
+    );
   }
 }
 

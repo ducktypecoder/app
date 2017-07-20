e2b9c980d2e8ffe9151e3124ee31bf79a77b7750
Mark Hurwitz <mhurwitzus@gmail.com>
2017-07-20 15:16:10 -0500

    Alters styles for Index page

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -7,20 +7,20 @@ import './Index.scss';
 function Index() {
   return (
     <div className="Index">
-      <img
-        src="http://www.animatedimages.org/data/media/481/animated-duck-image-0065.gif"
-        alt="little rubber ducky"
-      />
       <div className="hero">
         <h1>ducktypecoder</h1>
+        <img
+          src="http://www.animatedimages.org/data/media/481/animated-duck-image-0065.gif"
+          alt="little rubber ducky"
+        />
         <p>
           Practical software development tutorials guided by helpful automated
           tests.
         </p>
       </div>
       <div className="main-action">
-        <Link to="/projects/hello-world" className="btn btn-success">
-          &rarr; See an example project.
+        <Link to="/projects/hello-world" className="btn btn-success btn-lg">
+          &rarr; See an example project
         </Link>
       </div>
     </div>

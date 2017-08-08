d390b9b6e5ec37f0d0adbad579bcbc31cbb2543f
Mark Hurwitz <mhurwitzus@gmail.com>
2017-08-08 15:33:21 -0500

    Publish will update an existing project and insert a new project

--- imports/api/Projects/server/publish-project.js
+++ imports/api/Projects/server/publish-project.js
@@ -3,18 +3,38 @@ import slugify from '../../Utility/slugify';
 import getProjectConfigFromGithub from './get-project-config-from-github';
 import Projects from '../Projects';
 
+// https://blog.meteor.com/using-promises-and-async-await-in-meteor-8f6f4a04f998
 export default (async function publishProject(repo) {
   try {
     const projectConfig = await getProjectConfigFromGithub(repo);
-    const { projectName } = projectConfig;
-
-    // https://blog.meteor.com/using-promises-and-async-await-in-meteor-8f6f4a04f998
-    await Projects.rawCollection().insert({
-      _id: Random.id(),
+    const { projectName, author } = projectConfig;
+    const projectData = {
       gitRepo: repo,
       title: projectName,
       slug: slugify(projectName),
+      author,
+    };
+
+    // QUESTION: what if we want to find by the git repo, but change the name?
+    const existingProject = await Projects.rawCollection().findOne({
+      slug: slugify(projectName),
+      gitRepo: repo,
     });
+
+    // update the existing
+    if (existingProject) {
+      await Projects.rawCollection().update(
+        { _id: existingProject._id },
+        {
+          $set: projectData,
+        },
+      );
+      return { success: true };
+    }
+
+    // Else, create the new project
+    projectData._id = Random.id(); // using rawCollection, we need to manually set the document's _id
+    await Projects.rawCollection().insert(projectData);
 
     return { success: true };
   } catch (e) {

c357362a0c9f8c98e30e9781a6e65786f5a331d3
Mark Hurwitz <mhurwitzus@gmail.com>
2017-08-20 14:09:16 -0500

    Refactors and cleans up optimizely setup

--- imports/modules/optimizely.js
+++ imports/modules/optimizely.js
@@ -2,11 +2,6 @@ import { Meteor } from 'meteor/meteor';
 import axios from 'axios';
 import optimizely from 'optimizely-client-sdk';
 
-// TODO: grab datafile from optimizely api:
-// https://www.optimizelyapis.com/experiment/v1/projects/12345/json
-// authenticate the call:
-// https://developers.optimizely.com/classic/rest/getting-started/index.html
-
 // TODO: use settings to hide api token, get as ENV variable
 const API_TOKEN =
   'd6a2fe7b3cac448919167f6802da6810fcf9fd7e566413d6593fb27fd48d13f0:aQk2KnYxj';
@@ -18,16 +13,14 @@ const axiosClient = axios.create({
 
 async function getDatafile() {
   const response = await axiosClient.get('/json');
-  console.log('optimizely response: ', response);
+
   return response.data;
 }
 
 async function getOptimizelyClientInstance() {
   const datafile = await getDatafile();
 
-  return optimizely.createInstance({
-    datafile,
-  });
+  return optimizely.createInstance({ datafile });
 }
 
 async function setupExperiments() {
@@ -38,8 +31,6 @@ async function setupExperiments() {
     userId,
   );
   const headlineEl = document.getElementById('landing_page_headline');
-
-  console.log({ variation });
 
   switch (variation) {
     case '1':
--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -3,12 +3,9 @@ import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import App from '../../ui/layouts/App/App';
 import '../../ui/stylesheets/app.scss';
+import optimizely from '../../modules/optimizely';
 
 Meteor.startup(async () => {
   render(<App />, document.getElementById('react-root'));
-
-  const optimizelyImport = await import('../../modules/optimizely');
-  const optimizely = optimizelyImport.default;
-
   optimizely.setupExperiments();
 });
